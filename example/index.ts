d0acad2d5c0bfe5756100df1d9a24e2f53f07ad2
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2022-07-13 20:25:14 +0200

    Update example to work without createGame

--- example/index.ts
+++ example/index.ts
@@ -1,10 +1,42 @@
-import { createBoard, createGame } from '../src';
+import { createBoard } from '../src';
+import { createGame, Direction } from '2048-engine';
 
-const game = createGame({ onMoved: (state) => onMoved(state) });
-document.getElementById('game')?.appendChild(game.htmlElement);
+const game = createGame();
 
-const mirrorBoard = createBoard(game.currentState);
-const onMoved = mirrorBoard.update;
+const board = createBoard(game.currentState.board);
+document.getElementById('game')?.appendChild(board.htmlElement);
 
-mirrorBoard.htmlElement.style.width = '300px';
-document.getElementById('mirror')?.appendChild(mirrorBoard.htmlElement);
+const move = (direction: Direction) => {
+  const gameState = game.move(direction);
+  board.update(gameState.board);
+};
+
+window.addEventListener('keydown', (e) => {
+  switch (e.key) {
+    case 'ArrowUp':
+    case 'w': {
+      move(Direction.UP);
+      break;
+    }
+    case 'ArrowDown':
+    case 's': {
+      move(Direction.DOWN);
+      break;
+    }
+    case 'ArrowLeft':
+    case 'a': {
+      console.log('move left');
+      move(Direction.LEFT);
+      break;
+    }
+    case 'ArrowRight':
+    case 'd': {
+      move(Direction.RIGHT);
+      break;
+    }
+    default: {
+      return;
+    }
+  }
+  e.preventDefault();
+});
